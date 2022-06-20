ef1a9b56f092188b53daf1c591454090b6944f22
hesimire <hesimire@163.com>
2022-06-20 18:00:08 +0800

    uniapp前端接口开发完毕

--- routes/uniapp_shops.js
+++ routes/uniapp_shops.js
@@ -8,7 +8,14 @@ router.prefix('/shops')
 router.get('/', async function (ctx, next) {
   // ctx.body = 'this is a data response!'
   const ListNum = ctx.request.query.ListNum;
-  const List = await DB.findDocuments('shops', {})
+  const ListType = ctx.request.query.listType;
+  console.log(ListType);
+
+  const rgb = new RegExp("" + ListType + "")
+  const List = await DB.findDocuments('shops', {
+    cClass: rgb
+  })
+
   let startNum = (ListNum - 1) * 6;
   let limitNum = startNum + 5;
   let isOver = List.length > startNum
@@ -25,10 +32,41 @@ router.get('/', async function (ctx, next) {
   }
 })
 
-router.get('/swiperData', function (ctx, next) {
+// 查询商品
+router.get('/findByKeys', async function (ctx, next) {
+  // ctx.body = 'this is a data response!'
+  const ListNum = ctx.request.query.ListNum;
+  const ListType = ctx.request.query.listType;
 
+  const rgb = new RegExp("" + ListType + "")
+  const List = await DB.findDocuments('shops', {
+    cClass: rgb
+  })
 
+  console.log(List.length);
+  // let startNum = (ListNum - 1) * 6;
+  // let limitNum = startNum + 5;
+  // let isOver = List.length > startNum
+  // const dataArr = []
+  // // console.log(startNum, limitNum, isOver);
+  // for (; startNum <= limitNum && startNum <= List.length - 1; startNum++) {
+  //   dataArr.push(List[startNum])
+  // }
+  ctx.body = {
+    err_code: 0,
+    message: '查询成功',
+    // dataArr: dataArr,
+    // isOver: !isOver
+  }
+})
 
+router.get('/swiperData',async function (ctx, next) {
+  const List = await DB.findDocuments('swipper', { })
+  ctx.body = {
+    err_code: 0,
+    message: '查询成功',
+    dataArr: List,
+  }
 })
 
 module.exports = router
